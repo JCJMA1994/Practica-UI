8626d161e85a1def3aaf3068afa92e14cd76493f
JCJMA1994 <201214026@uns.edu.pe>
2021-06-12 19:42:24 -0500

    Interfaz Locacion agregada

--- app/src/main/java/com/example/practica_demo/ui/location/LocationViewModel.kt
+++ app/src/main/java/com/example/practica_demo/ui/location/LocationViewModel.kt
@@ -6,7 +6,7 @@ import androidx.lifecycle.ViewModel
 
 class LocationViewModel : ViewModel(){
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "Mapa de Ubicacion"
     }
     val text: LiveData<String> = _text
 }
